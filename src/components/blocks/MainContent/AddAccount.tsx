5629e3272130365a9fc14c4481ba0f2d7c2c78bc
Stephen-Ibe <saintkalson@gmail.com>
2022-10-31 22:32:16 +0100

    add wallet modal and API call

--- src/components/blocks/MainContent/AddAccount.tsx
+++ src/components/blocks/MainContent/AddAccount.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Modal from '../../shared/Modal';
 
 type Props = {
@@ -6,6 +6,61 @@ type Props = {
 };
 
 const AddAccount = ({ handleClose }: Props) => {
+  const [wallets, setWallets] = useState<any>([]);
+  const [formData, setFormData] = useState('');
+  const [error, setError] = useState<string>('');
+
+  const handleInputChange = (e: any) => {
+    const { value } = e.target;
+    setFormData(value);
+    setError('');
+  };
+
+  const addWallet = async (currency: string) => {
+    const payload = {
+      currency,
+    };
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload),
+    };
+    try {
+      const res = await fetch('http://localhost:3090/accounts', requestOptions);
+      console.log(res);
+      const jsonResponse = await res.json();
+      console.log(jsonResponse);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (formData === 'Choose a wallet' || formData === '') {
+      setError('Please choose an option');
+    } else {
+      setError('');
+      console.log(formData);
+      addWallet(formData);
+    }
+  };
+
+  const fetchWallets = async () => {
+    try {
+      const res = await fetch('http://localhost:3090/wallets');
+      const jsonResponse = await res.json();
+      setWallets(jsonResponse);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchWallets();
+  }, []);
+
   return (
     <Modal isOpen>
       <div className='addWallet'>
@@ -23,9 +78,24 @@ const AddAccount = ({ handleClose }: Props) => {
           <form>
             <div className='select-field'>
               <label>Select Wallet</label>
-              <select>
-                <option>Hello</option>
+              <select
+                value={formData}
+                onChange={handleInputChange}
+                name='wallet'
+                defaultValue={'default'}
+              >
+                <option>Choose a wallet</option>
+                {wallets.length > 0 &&
+                  wallets.map(({ currency, name }: any) => (
+                    <option id={currency} value={currency}>
+                      {name}
+                    </option>
+                  ))}
               </select>
+              <p className='error__msg'>{error}</p>
+            </div>
+            <div className='button' onClick={handleSubmit}>
+              <button type='submit'>Create Wallet</button>
             </div>
           </form>
         </div>
