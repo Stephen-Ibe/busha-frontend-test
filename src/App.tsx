cdd5f3dae6ca6583fe346ce69fe465ae320e8f81
Stephen-Ibe <saintkalson@gmail.com>
2022-10-31 17:49:32 +0100

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import MainContent from './components/blocks/MainContent';
+import SideBar from './components/blocks/Sidebar';
 import TopNav from './components/blocks/TopNav';
 
 function App() {
   return (
     <>
       <TopNav />
-      <h1>Busha</h1>
+      <div className='main mx-auto container'>
+        <SideBar />
+        <MainContent />
+      </div>
     </>
   );
 }
--- src/components/blocks/Sidebar/index.tsx
+++ src/components/blocks/Sidebar/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const navItems = [
+    { id: 0, name: 'Wallets', isActive: true },
+    { id: 1, name: 'Prices' },
+    { id: 2, name: 'Peer2Peer' },
+    { id: 3, name: 'Activity' },
+    { id: 4, name: 'Settings' },
+  ];
+
+  return (
+    <div className='sidebar'>
+      <ul>
+        {navItems.map((item: any) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
