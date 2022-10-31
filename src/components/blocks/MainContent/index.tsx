1e0c54beb847e2bcd6cad7d82fcff4373a64d6ca
Stephen-Ibe <saintkalson@gmail.com>
2022-10-31 17:56:17 +0100

    main content

--- src/components/blocks/MainContent/index.tsx
+++ src/components/blocks/MainContent/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-
 const MainContent = () => {
-  return <div>MainContent</div>;
+  return (
+    <div className='main__content'>
+      <div className='heading'>
+        <h2>Wallets</h2>
+      </div>
+    </div>
+  );
 };
 
 export default MainContent;
