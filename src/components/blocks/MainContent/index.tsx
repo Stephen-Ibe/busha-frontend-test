3435c446455649f4d97a8f371f035ee8ab808b95
Stephen-Ibe <saintkalson@gmail.com>
2022-10-31 18:21:17 +0100

    loading spinner when fetch accounts from API

--- src/components/blocks/MainContent/index.tsx
+++ src/components/blocks/MainContent/index.tsx
@@ -1,13 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Loader from '../../shared/Loader';
 
 const MainContent = () => {
+  const [accounts, setAccounts] = useState<{ [key: string]: any }[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
   const fetchAccounts = async () => {
+    setLoading(true);
     try {
       const res = await fetch('http://localhost:3090/accounts');
       const jsonResponse = await res.json();
       console.log(jsonResponse);
+      setAccounts(jsonResponse);
     } catch (err: any) {
       console.log(err);
+    } finally {
+      setLoading(true);
     }
   };
 
@@ -20,7 +28,15 @@ const MainContent = () => {
       <div className='heading'>
         <h2>Wallets</h2>
       </div>
-      <div className='content'></div>
+      <div className='content'>
+        {loading ? (
+          <div className='center__element'>
+            <Loader size={50} width={5} />
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   );
 };
