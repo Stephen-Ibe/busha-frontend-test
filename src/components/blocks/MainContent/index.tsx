0a89e743479aae07c90e154deb06ec8ed1cc439e
Stephen-Ibe <saintkalson@gmail.com>
2022-10-31 18:06:42 +0100

    fecth accounts from API

--- src/components/blocks/MainContent/index.tsx
+++ src/components/blocks/MainContent/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const MainContent = () => {
+  const fetchAccounts = async () => {
+    try {
+      const res = await fetch('http://localhost:3090/accounts');
+      const jsonResponse = await res.json();
+      console.log(jsonResponse);
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchAccounts();
+  }, []);
+
   return (
     <div className='main__content'>
       <div className='heading'>
         <h2>Wallets</h2>
       </div>
+      <div className='content'></div>
     </div>
   );
 };
